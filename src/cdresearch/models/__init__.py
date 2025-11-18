b8078f6dbf209186159b0946adaa7fa65ad16c47
regencode <thomgozalie@gmail.com>
2025-11-18 14:03:17 +0700

    fix

--- src/cdresearch/models/__init__.py
+++ src/cdresearch/models/__init__.py
@@ -1,6 +1,5 @@
 from .fc_cd import FC_EF
 from .hrscd import HRSCD_str4
 from .changeformer import ChangeFormer
-from .mambavision_cd.mambavision_cd_models.mamba_fusion import MambaVisionCD
 
-__all__ = ["FC_EF", "HRSCD_str4", "ChangeFormer", "MambaVisionCD"]
+__all__ = ["FC_EF", "HRSCD_str4", "ChangeFormer"]
