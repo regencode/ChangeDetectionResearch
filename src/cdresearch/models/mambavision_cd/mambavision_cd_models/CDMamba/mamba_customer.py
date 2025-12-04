8e596f99c10748fe2bff0649de9ff5709610d046
regencode <thomgozalie@gmail.com>
2025-12-04 12:01:47 +0700

    fix

--- src/cdresearch/models/mambavision_cd/mambavision_cd_models/CDMamba/mamba_customer.py
+++ src/cdresearch/models/mambavision_cd/mambavision_cd_models/CDMamba/mamba_customer.py
@@ -16,7 +16,7 @@ from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, mamba_inne
 
 from mamba_ssm.ops.triton.selective_state_update import selective_state_update
 
-from mamba_ssm.ops.triton.layernorm import RMSNorm, layer_norm_fn, rms_norm_fn
+from mamba_ssm.ops.triton.layer_norm import RMSNorm, layer_norm_fn, rms_norm_fn
 
 class LightweightModel(nn.Module):
     def __init__(self, in_channels, out_channels):
