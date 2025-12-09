8f0ef34d24e6604cef0ad29121b3c06985ba7b44
regencode <thomgozalie@gmail.com>
2025-12-09 15:38:32 +0700

    feat: conf matrix

--- src/cdresearch/utils/metrics.py
+++ src/cdresearch/utils/metrics.py
@@ -1,4 +1,5 @@
 import torch
+import einops as ein
 
 def calculate_metrics(y_true, y_pred, is_binary=True):
     '''
@@ -17,8 +18,35 @@ def calculate_metrics(y_true, y_pred, is_binary=True):
     precision = TP / (TP + FP) # how correct given a prediction of a class
     recall = TP / (TP + FN) # how much of a class is accurately "recalled"
     F1 = (2 * precision * recall) / (precision + recall) # harmonic mean of precision and recall (???)
+
     IoU = TP / (TP + FP + FN) # how much overlap are there between prediction and label
 
     return accuracy.item(), precision.item(), recall.item(), F1.item(), IoU.item()
+
+
+def get_confusion_matrix(y_true, y_pred, is_binary=True):
+    '''
+    Calculate and return accuracy, precision, recall, F1, IoU
+    '''
+    assert y_true.shape == y_pred.shape, f"y_true shape={y_true.shape} != y_pred shape{y_pred.shape}"
+    if is_binary:
+        TP = torch.sum((y_true == 1) & (y_pred == 1))
+        TN = torch.sum((y_true == 0) & (y_pred == 0))
+        FP = torch.sum((y_true == 0) & (y_pred == 1))
+        FN = torch.sum((y_true == 1) & (y_pred == 0))
+    else:
+        print("Not implemented")
+        return -1
+    print(TP, TN, FP, FN)
+    conf_array = torch.tensor([TP, FN, FP, TN])
+    conf_mat = conf_array.view((2, 2)).numpy()
+    return conf_mat
+
+if __name__ == "__main__":
+    N, W, H = 2, 20, 20
+    true = torch.randint(0, 2, (N, W, H))
+    pred = torch.randint(0, 2, (N, W, H))
+    print(get_confusion_matrix(true, pred))
+
 
 
