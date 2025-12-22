0aca5f02278960481843d3354fd319c48e444846
regencode <thomgozalie@gmail.com>
2025-12-22 13:20:46 +0700

    feat: dataset will clone data to the same folder of module for standardized access

--- src/cdresearch/datasets/whu_cd.py
+++ src/cdresearch/datasets/whu_cd.py
@@ -3,32 +3,34 @@ import shutil
 import zipfile
 import torch
 import cv2 as cv
-from .base_dataset import BaseDataset, Patchify
 from torchvision.utils import save_image
+from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+from .base_dataset import BaseDataset, Patchify
 
 def load_whu(drive_path, patchify=False, patch_size=(256, 256)):
+    MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
     DATA_SOURCE = drive_path
-    DATA_DEST = "./WHU_CD"
-    DATA_PATCH_FOLDER = "./WHU_CD_PATCHED/"
+    DATA_DEST = f"{MODULE_DIR}/WHU_CD"
+    DATA_PATCH_FOLDER = f"{MODULE_DIR}/WHU_CD_PATCHED/"
 
     if os.path.exists(DATA_DEST):
-        print("Data patch folder already exists! Skipping loading and unzipping data...")
-        return
-
-    os.makedirs(DATA_DEST)
-    dest = shutil.copy(DATA_SOURCE, DATA_DEST)
-    with zipfile.ZipFile(dest, 'r') as z:
-        z.extractall(DATA_DEST + "/" + dest.split("/")[-1][:-4])
-    print("Data load and unzip complete")
+        print("Data unzip dest folder already exists! Skipping loading data...")
+    else: 
+        os.makedirs(DATA_DEST)
+        dest = shutil.copy(DATA_SOURCE, DATA_DEST)
+        with zipfile.ZipFile(dest, 'r') as z:
+            z.extractall(DATA_DEST + "/" + dest.split("/")[-1][:-4])
+        print("Data load and unzip complete")
 
     if not patchify:
         print("Skip patchify data (patchify == False)")
         return
 
     patcher = Patchify(*patch_size)
-    if os.path.exists(DATA_PATCH_FOLDER):
-        print("Data patch folder already exists! Skipping patchify...")
-        return
+    #if os.path.exists(DATA_PATCH_FOLDER):
+    #    print("Data patch folder already exists! Skipping patchify...")
+    #    return
     os.makedirs(DATA_PATCH_FOLDER)
     os.makedirs(DATA_PATCH_FOLDER + "train/")
     os.makedirs(DATA_PATCH_FOLDER + "test/")
@@ -37,11 +39,13 @@ def load_whu(drive_path, patchify=False, patch_size=(256, 256)):
         os.makedirs(split_path + "/" + "A/")
         os.makedirs(split_path + "/" + "B/")
         os.makedirs(split_path + "/" + "label/")
+        image_path_prefix = os.path.join(DATA_DEST, f"Building change detection dataset_add/1. The two-period image data/")
         image_dict = {
-            "A": os.path.join(DATA_DEST, f"/1. The two-period image data/2012/whole_image/{split}/image/2012_{split}.tif"),
-            "B": os.path.join(DATA_DEST, f"/1. The two-period image data/2016/whole_image/{split}/image/2016_{split}.tif"),
-            "label": os.path.join(DATA_DEST, f"1. The two-period image data/change_label/{split}/change_label.tif")
+            "A": os.path.join(image_path_prefix, f"2012/whole_image/{split}/image/2012_{split}.tif"),
+            "B": os.path.join(image_path_prefix, f"2016/whole_image/{split}/image/2016_{split}.tif"),
+            "label": os.path.join(image_path_prefix, f"change_label/{split}/change_label.tif")
         }
+        print(image_dict)
         for subsplit in os.listdir(split_path):
             subsplit_path =  os.path.join(split_path, subsplit)
             image_path = image_dict[subsplit]
@@ -69,6 +73,3 @@ class WHU_CD_Dataset(BaseDataset):
         mask_paths = glob.glob(f"{mask_dir}/*.png")
 
         super().__init__(x1_paths, x2_paths, mask_paths, pair_transforms, return_y_image)
-
-
-
