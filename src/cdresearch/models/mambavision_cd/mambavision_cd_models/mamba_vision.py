16c142bc53729b82345f54522178b14b12ce4729
regencode <thomgozalie@gmail.com>
2025-12-25 10:06:27 +0700

    layernorm to initial downsampling

--- src/cdresearch/models/mambavision_cd/mambavision_cd_models/mamba_vision.py
+++ src/cdresearch/models/mambavision_cd/mambavision_cd_models/mamba_vision.py
@@ -295,7 +295,7 @@ class PatchEmbed(nn.Module):
         self.proj = nn.Identity()
         if downsample:
             self.conv_down = nn.Sequential(
-                nn.Conv2d(in_chans, out_chans, 3, 2, 1, bias=False), # kernel stride padding
+                nn.Conv2d(in_chans, in_dim, 3, 2, 1, bias=False), # kernel stride padding
                 nn.BatchNorm2d(out_chans, eps=1e-4),
                 nn.ReLU(),
                 nn.Conv2d(in_dim, out_chans, 3, 2, 1, bias=False),
