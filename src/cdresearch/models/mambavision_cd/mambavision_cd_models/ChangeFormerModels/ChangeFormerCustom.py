aa7744eeff80b69b144c815221358bc5dcec4472
regencode <thomgozalie@gmail.com>
2025-12-25 21:15:45 +0700

    feat: change self.linear_fuse to use multilevelfuse for more capacity and depth

--- src/cdresearch/models/mambavision_cd/mambavision_cd_models/ChangeFormerModels/ChangeFormerCustom.py
+++ src/cdresearch/models/mambavision_cd/mambavision_cd_models/ChangeFormerModels/ChangeFormerCustom.py
@@ -130,21 +130,30 @@ class MultiLevelFuse(nn.Module):
     def __init__(self, embedding_dim=256):
         super().__init__()
         self.layer3 = nn.Sequential(
-            ResidualBlock(embedding_dim),
+            ResidualBlock(embedding_dim*2),
+            nn.Conv2d(embedding_dim*2, embedding_dim, 3, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(embedding_dim),
         )
         self.layer2 = nn.Sequential(
             ResidualBlock(embedding_dim),
+            nn.Conv2d(embedding_dim*2, embedding_dim, 3, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(embedding_dim),
         )
         self.layer1 = nn.Sequential(
             ResidualBlock(embedding_dim),
+            nn.Conv2d(embedding_dim*2, embedding_dim, 3, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(embedding_dim),
         )
     def forward(self, x):
         # x is c4, c3, c2, c1 concatenated at dim=1
         N, C, H, W = x.shape 
         x4, x3, x2, x1 = x.reshape(4, N, -1, H, W)
-        _c3 = self.layer3(torch.add(x4, x3))
-        _c2 = self.layer2(torch.add(_c3, x2))
-        _c1 = self.layer1(torch.add(_c2, x1))
+        _c3 = self.layer3(torch.cat([x4, x3], dim=1))
+        _c2 = self.layer2(torch.cat([_c3, x2], dim=1))
+        _c1 = self.layer1(torch.cat([_c2, x1], dim=1))
         return _c1
 
 class DecoderTransformerCustom(DecoderTransformer_v3):
